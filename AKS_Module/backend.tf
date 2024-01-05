97eb25b5d98f35706c424da528fd39e423a778c9
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-05 22:15:22 +0530

    Changed Name

--- AKS_Module/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Terraform-State-RG"
-    storage_account_name = "terraformstate54321"
-    container_name       = "gowebserver"
-    key                  = "terraform.tf"
-    # access_key = var.STORAGE_KEY
-  }
-}
